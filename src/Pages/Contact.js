61167792ab6832d9bfa46d0e48490ea6ca2d4b7e
Joshua Barua <joshuabarua9@hotmail.com>
2024-01-15 12:25:37 +0100

    Make form contact functional

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,57 +1,52 @@
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
 import emailjs from '@emailjs/browser';
 
 const ContactForm = () => {
+	const formRef = useRef();
 	const [formData, setFormData] = useState({
-		name: '',
-		email: '',
+		from_name: '',
+		from_email: '',
+		subject: '',
 		message: '',
+		to_name: 'Joshua Barua-Fowle',
 	});
 
-	const handleChange = (e) => {
+	const handleInputChange = (e) => {
 		const {name, value} = e.target;
-		setFormData((prevData) => ({
-			...prevData,
+		setFormData((prevState) => ({
+			...prevState,
 			[name]: value,
 		}));
 	};
 
-	const handleSubmit = (e) => {
+	const handleSubmit = async (e) => {
 		e.preventDefault();
+		try {
+			const result = await emailjs.sendForm(process.env.REACT_APP_SERVICE_ID, process.env.REACT_APP_TEMPLATE_ID, formRef.current, process.env.REACT_APP_PUBLIC_KEY);
+			console.log('Form sent successfully:', result.text);
 
-		emailjs
-			.send(
-				'YOUR_SERVICE_ID', // Replace with your EmailJS service ID
-				'YOUR_TEMPLATE_ID', // Replace with your EmailJS template ID
-				e.target,
-				'YOUR_USER_ID' // Replace with your EmailJS user ID
-			)
-			.then(
-				(result) => {
-					console.log('Email sent successfully:', result.text);
-					// Reset form after successful submission
-					setFormData({
-						name: '',
-						email: '',
-						message: '',
-					});
-				},
-				(error) => {
-					console.error('Error sending email:', error.text);
-				}
-			);
+			setFormData({
+				from_name: '',
+				from_email: '',
+				subject: '',
+				message: '',
+				to_name: 'Joshua Barua-Fowle',
+			});
+		} catch (error) {
+			console.error('Failed to send form:', error);
+		}
 	};
 
 	return (
 		<main className="h-screen w-[100%] flex justify-end items-end flex-col ">
 			<section className=" w-[60%] pb-[100px]">
 				<h1 className="text-3xl  pb-1  dark-text dark:light-text md:text-2xl xl:text-4xl xl:leading-tight font-bold">Contact</h1>
-				<form onSubmit={handleSubmit} className="flex  flex-col justify-center items-left gap-2 w-[50%] h-[550px] min-w-[280px] max-w-[500px] mr-2 rounded-md">
+				<form onSubmit={handleSubmit} ref={formRef} className="flex  flex-col justify-center items-left gap-2 w-[50%] h-[550px] min-w-[280px] max-w-[500px] mr-2 rounded-md">
 					{/* Name */}
-					<label for="email-name" className="block  text-sm font-medium dark-text dark:light-text">
+					<label htmlFor="email-name" className="block  text-sm font-medium dark-text dark:light-text">
 						Name
 					</label>
-					<div class="relative mb-2">
+					<div className="relative mb-2">
 						<div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
 							<svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
 								<path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z" />
@@ -60,12 +55,15 @@ const ContactForm = () => {
 						<input
 							type="text"
 							id="email-name"
-							class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-slate-100 focus:border-slate-100 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-slate-100 dark:focus:border-slate-100"
+							name="from_name"
+							value={formData.from_name}
+							onChange={handleInputChange}
+							className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-slate-100 focus:border-slate-100 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-slate-100 dark:focus:border-slate-100"
 							placeholder="John Smith"
 						/>
 					</div>
 					{/* EMAIL */}
-					<label for="email-address" class="block text-sm font-medium dark-text dark:light-text">
+					<label htmlFor="email-address" className="block text-sm font-medium dark-text dark:light-text">
 						Email
 					</label>
 					<div className="relative mb-2">
@@ -78,37 +76,48 @@ const ContactForm = () => {
 						<input
 							type="text"
 							id="email-address"
+							name="from_email"
+							value={formData.from_email}
+							onChange={handleInputChange}
 							className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 							placeholder="john@smith.com"
 						/>
 					</div>
 					{/* Subject */}
-					<label for="email-subject" class="block text-sm font-medium dark-text dark:light-text">
+					<label htmlFor="email-subject" className="block text-sm font-medium dark-text dark:light-text">
 						Subject
 					</label>
 					<div className="relative mb-2">
 						<div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
-							<svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 16">
-								<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h14M1 6h14M1 11h7" />
+							<svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 16">
+								<path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h14M1 6h14M1 11h7" />
 							</svg>
 						</div>
 						<input
 							type="text"
 							id="email-subject"
+							name="subject"
+							value={formData.subject}
+							onChange={handleInputChange}
 							className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 							placeholder="Lets work together!"
 						/>
 					</div>
 					<div>
-						<label for="message" class="block mb-2 text-sm font-medium dark-text dark:light-text">
+						<label htmlFor="message" className="block mb-2 text-sm font-medium dark-text dark:light-text">
 							Message
 						</label>
 						<textarea
 							id="message"
+							name="message"
 							rows="4"
-							class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+							value={formData.message}
+							onChange={handleInputChange}
+							className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 							placeholder="Write your thoughts here..."></textarea>
 					</div>
+					<input type="hidden" name="to_name" value="Joshua Barua-Fowle" />
+
 					<button type="submit" className="h-[40px] w-[100%] rounded-md p-3 bg-gray-700 ">
 						Send
 					</button>
