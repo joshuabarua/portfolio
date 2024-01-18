3068b68648f4c3080d4fc7e3ce7b594ceeb274de
Joshua Barua <joshuabarua9@hotmail.com>
2024-01-18 14:04:58 +0100

    styling for mobile

--- src/Pages/Mobile/AboutMobile.jsx
+++ src/Pages/Mobile/AboutMobile.jsx
@@ -4,14 +4,14 @@ import {personalDetails} from '../../Details';
 function AboutMobile() {
 	const {name, tagline, about} = personalDetails;
 	return (
-		<div className="h-screen w-[100%] flex justify-center items-center flex-col ">
-			<div className="pt-10 w-[70%]">
-				<h1 className=" pt-10 wrapped-text  text-dark-text dark:text-light-text text-3xl ">{name}</h1>
-				<h2 className="wrapped-text text-dark-text dark:text-light-text text-lg ">{tagline}</h2>
+		<div className="h-screen w-[100%] flex justify-center items-center flex-col overflow-y-auto my-3">
+			<div className="w-[70%]  overflow-y-hidden">
+				<h1 className=" pt-10 wrapped-text  text-dark-text dark:text-light-text text-xl ">{name}</h1>
+				<h2 className="wrapped-text text-dark-text dark:text-light-text text-md ">{tagline}</h2>
 			</div>
-			<div className=" w-[70%]  max-w-[350px] pt-[80px] overflow-y-auto mb-[50px] flex gap-5 flex-col">
-				<h1 className="wrapped-text text-center text-xl text-dark-text dark:text-light-text md:text-2xl xl:text-3xl xl:leading-tight font-bold">About Me</h1>
-				<p className="wrapped-text text-dark-text dark:text-light-text text-sm leading-6 md:text-md lg:text-lg">{about}</p>
+			<div className=" w-[70%] pt-[20px] flex gap-2 flex-col overflow-y-auto">
+				<h1 className="wrapped-text text-center text-lg text-dark-text dark:text-light-text md:text-2xl xl:text-3xl xl:leading-tight font-bold  ">About Me</h1>
+				<p className="wrapped-text text-dark-text dark:text-light-text text-sm leading-6 md:text-md lg:text-lg  ">{about}</p>
 			</div>
 		</div>
 	);
