d30ef28b557ccfab7576863445587d40b72faa01
Joshua Barua <joshuabarua9@hotmail.com>
2024-04-03 15:15:13 +0200

    Adjust mobile about

--- src/Pages/Mobile/AboutMobile.jsx
+++ src/Pages/Mobile/AboutMobile.jsx
@@ -4,11 +4,11 @@ import {personalDetails} from '../../data/details.js';
 function AboutMobile() {
 	const {about} = personalDetails;
 	return (
-		<div className="h-screen w-[100%] flex justify-center items-center flex-col my-[20px] py-[20px] ml-5">
+		<div className="h-screen w-[100%] flex justify-center items-center flex-col my-[20px] py-[20px]">
 			<h1 className="wrapped-text text-2xl text-dark-text dark:text-light-text page-title">About Me</h1>
 
-			<div className="px-[6rem] flex gap-2 flex-col ">
-				<p className="wrapped-text text-dark-text dark:text-light-text text-sm leading-6 md:text-md lg:text-lg  pb-8">{about}</p>
+			<div className="px-[2rem] flex flex-col ">
+				<p className="wrapped-text text-dark-text dark:text-light-text text-sm leading-tight md:text-md lg:text-lg pb-8">{about}</p>
 			</div>
 		</div>
 	);
