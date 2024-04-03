24805c70fb60566d2d3ac8479ccbc61c4ebca9cf
Joshua Barua <joshuabarua9@hotmail.com>
2024-04-03 13:55:27 +0200

    adjust styling for projects view

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -6,7 +6,7 @@ function Projects() {
 	return (
 		<>
 			<h1 className="text-2xl dark-text dark:light-text page-title">Projects</h1>
-			<main className="h-screen w-[100%] flex justify-start items-center flex-col mt-[100px] mb-[-125px] pb-[30px]">
+			<main className="h-screen w-[100%] flex justify-start items-center flex-col mt-[50px] mb-[-82px]">
 				<div className="projectCardGrid">{React.Children.toArray(projectDetails.map(({...projectDetails}, idx) => <Project props={projectDetails} idx={idx} />))}</div>
 			</main>
 		</>
