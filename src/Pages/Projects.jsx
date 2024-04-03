bb2f668fa3c10a99047d2ccb8090273e83c10315
Joshua Barua <joshuabarua9@hotmail.com>
2024-04-03 13:42:07 +0200

    Fix resizing of grid

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -6,7 +6,7 @@ function Projects() {
 	return (
 		<>
 			<h1 className="text-2xl dark-text dark:light-text page-title">Projects</h1>
-			<main className="h-screen w-[65%] flex justify-start items-center flex-col mt-[31px]">
+			<main className="h-full w-[100%] flex justify-center items-center flex-col mt-[50px] ">
 				<div className="projectCardGrid">{React.Children.toArray(projectDetails.map(({...projectDetails}, idx) => <Project props={projectDetails} idx={idx} />))}</div>
 			</main>
 		</>
