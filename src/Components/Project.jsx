4815f0c37fac96afce9e44f36cc858545e2411ff
Joshua Barua <joshuabarua9@hotmail.com>
2024-04-03 14:09:57 +0200

    Mobile media query for small screens on projects

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -36,7 +36,7 @@ function Project({props, idx}) {
 		<article className="projectCard" onClick={handleCardClick}>
 			<img src={image} alt="" loading={lazy} />
 			<h1 className="dark-text wrapped-text break-all dark:light-text font-semibold text-lg lg:text-xl xl:text-2xl">{title}</h1>
-			<div className="details wrapped-text px-10">
+			<div className="details wrapped-text px-2 md:px-12">
 				<p className="wrapped-text font-light text-md  lg:text-lg xl:text-xl">{description}</p>
 				<h3 className="dark-text dark:light-text  pt-2 ">
 					<strong>Tech Stack: </strong>
