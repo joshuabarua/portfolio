7c13c06d8b3bcd03488f274ed227b986166435dd
Joshua Barua <joshuabarua9@hotmail.com>
2024-04-08 19:04:47 +0200

    fix styles for projects page

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -6,7 +6,7 @@ function Projects() {
 	return (
 		<>
 			<h1 className="text-md mr-[-1px] md:text-2xl dark-text dark:light-text page-title">Projects</h1>
-			<main className="h-screen w-[100%] flex justify-start items-center flex-col mt-[50px] mb-[-82px]">
+			<main className="h-screen w-[100%] flex justify-start items-center flex-col mt-[70px] mb-[-102px] pt-10">
 				<div className="projectCardGrid">{React.Children.toArray(projectDetails.map(({...projectDetails}, idx) => <Project props={projectDetails} idx={idx} />))}</div>
 			</main>
 		</>
