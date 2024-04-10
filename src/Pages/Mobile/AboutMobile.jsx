a6c1b39da529396432263e264160541363f73549
Joshua Barua <joshuabarua9@hotmail.com>
2024-04-10 21:42:15 +0200

    Margin bottom about

--- src/Pages/Mobile/AboutMobile.jsx
+++ src/Pages/Mobile/AboutMobile.jsx
@@ -6,7 +6,7 @@ function AboutMobile() {
 	return (
 		<>
 			<h1 className="wrapped-text text-md mr-[-1px] md:text-2xl text-dark-text dark:text-light-text page-title">About Me</h1>
-			<div className="w-full flex items-end flex-col-reverse mb-[10%] h-xs:mb-[20%] h-sm:mb-[35%] h-md:mb-[40%] sm:mb-[35%] overflow-hidden">
+			<div className="w-full flex items-end flex-col-reverse  h-xs:mb-[30%] h-sm:mb-[45%] h-md:mb-[50%] sm:mb-[45%] overflow-y-hidden">
 				<div className="flex items-end flex-col-reverse w-5/6 overflow-hidden ">
 					<p className="wrapped-text text-dark-text backdrop-opacity-30 backdrop-blur-xl dark:text-light-text text-xs pr-4 leading-tight h-md:text-base sm:text-sm md:text-base pb-18 sm:pr-16">
 						{about}
