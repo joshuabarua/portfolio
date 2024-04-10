2e28b2bdb63f8ec1c327c64541615b23f41adddc
Joshua Barua <joshuabarua9@hotmail.com>
2024-04-10 18:10:47 +0200

    Styling mobile about page

--- src/Pages/Mobile/AboutMobile.jsx
+++ src/Pages/Mobile/AboutMobile.jsx
@@ -6,9 +6,9 @@ function AboutMobile() {
 	return (
 		<>
 			<h1 className="wrapped-text text-md mr-[-1px] md:text-2xl text-dark-text dark:text-light-text page-title">About Me</h1>
-			<div className="w-[100%] flex justify-center items-center flex-col">
-				<div className="px-[5rem] flex flex-col justify-center items-center">
-					<p className="wrapped-text text-dark-text backdrop-opacity-30 backdrop-blur-xl dark:text-light-text text-base leading-tight md:text-md lg:text-lg pb-8">{about}</p>
+			<div className="w-[100%] flex items-end flex-col-reverse">
+				<div className="flex items-end flex-col-reverse w-5/6 ">
+					<p className="wrapped-text text-dark-text backdrop-opacity-30 backdrop-blur-xl dark:text-light-text text-sm leading-tight sm:text-lg pb-4 pr-4">{about}</p>
 				</div>
 			</div>
 		</>
