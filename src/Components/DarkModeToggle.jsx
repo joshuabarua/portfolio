b09402017be2c532f41fa3f296c060339e1b6065
Joshua Barua <joshuabarua9@hotmail.com>
2024-04-10 22:14:47 +0200

    Dark light toggle readjustment

--- src/Components/DarkModeToggle.jsx
+++ src/Components/DarkModeToggle.jsx
@@ -13,17 +13,13 @@ export const DarkModeToggle = () => {
 			<div className="lightdark gap-4">
 				<div className="baseline">
 					<input type="checkbox" id="light-checkbox" className="custom-checkbox" checked={!isDark} onChange={handleToggle} />
-					<div className="custom-box" onClick={handleToggle}>
-						{!isDark ? '■' : '□'}
-					</div>
-					<label htmlFor="light-checkbox">LIGHT</label>
+					<div className={`custom-box  ${!isDark ? 'bg-[#1a1a1a]' : ' border-[1px] opacity-70'}`} onClick={handleToggle}></div>
+					<h5 htmlFor="light-checkbox">LIGHT</h5>
 				</div>
 				<div className="baseline">
 					<input type="checkbox" id="dark-checkbox" className="custom-checkbox" checked={isDark} onChange={handleToggle} />
-					<div className="custom-box" onClick={handleToggle}>
-						{isDark ? '■' : '□'}
-					</div>
-					<label htmlFor="dark-checkbox">DARK</label>
+					<div className={`custom-box  ${isDark ? 'bg-[#f2f2f2]' : 'border-[1px] opacity-70 border-dark-color border-solid'}`} onClick={handleToggle}></div>
+					<h5 htmlFor="dark-checkbox">DARK</h5>
 				</div>
 			</div>
 		</>
