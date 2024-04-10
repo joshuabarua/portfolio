48143121648e3fffc37ec7867a49a7691e1e0aa4
Joshua Barua <joshuabarua9@hotmail.com>
2024-04-10 20:50:45 +0200

    margin and padding

--- src/Pages/Mobile/AboutMobile.jsx
+++ src/Pages/Mobile/AboutMobile.jsx
@@ -6,9 +6,9 @@ function AboutMobile() {
 	return (
 		<>
 			<h1 className="wrapped-text text-md mr-[-1px] md:text-2xl text-dark-text dark:text-light-text page-title">About Me</h1>
-			<div className="w-full flex items-end flex-col-reverse mb-[10%] overflow-hidden">
-				<div className="flex items-end flex-col-reverse w-5/6 overflow-hidden">
-					<p className="wrapped-text text-dark-text backdrop-opacity-30 backdrop-blur-xl dark:text-light-text text-sm leading-tight sm:text-lg pb-6 pr-6">{about}</p>
+			<div className="w-full flex items-end flex-col-reverse mb-[20%] overflow-hidden">
+				<div className="flex items-end flex-col-reverse w-5/6 overflow-hidden ">
+					<p className="wrapped-text text-dark-text backdrop-opacity-30 backdrop-blur-xl dark:text-light-text text-sm leading-tight sm:text-lg pb-18 pr-6">{about}</p>
 				</div>
 			</div>
 		</>
