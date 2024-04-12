dc8b75b60261b9d49bab79dd23997ebcecc04b10
Joshua Barua <joshuabarua9@hotmail.com>
2024-04-12 17:12:35 +0200

    add about1440p font to border

--- src/Pages/1440p/About1440.jsx
+++ src/Pages/1440p/About1440.jsx
@@ -3,12 +3,14 @@ import {personalDetails} from '../../data/details.js';
 
 function About1440() {
 	return (
-		<div className="h-screen w-[100%] flex justify-end items-end flex-col about">
-			<div className=" w-[50%] h-{100%} min-w-[250px] max-w-[500px] overflow-y-auto mb-[10%] mr-[10%]">
-				<h1 className="wrapped-text text-left text-dark-text dark:text-light-text text-6xl xl:leading-tight font-bold">About Me</h1>
-				<p className="wrapped-text text-dark-text dark:text-light-text text-3xl leading-10 ">{personalDetails.about}</p>
+		<>
+			<h1 className="dark-text dark:light-text text-2xl xl:leading-tight page-title ">About Me</h1>
+			<div className="h-screen w-[100%] flex justify-end items-end flex-col about">
+				<div className=" w-[50%] h-{100%} min-w-[250px] max-w-[500px] overflow-y-auto mb-[10%] mr-[10%]">
+					<p className="wrapped-text text-dark-text dark:text-light-text text-3xl leading-10 ">{personalDetails.about}</p>
+				</div>
 			</div>
-		</div>
+		</>
 	);
 }
 
